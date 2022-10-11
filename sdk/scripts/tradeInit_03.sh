c80068015ec1d6be9b781d9ff7b4c8eff59c0411
Haodong Lu <79371536+SleepyLGod@users.noreply.github.com>
2022-10-11 15:21:47 +0800

    Update tradeInit_03.sh

--- sdk/scripts/tradeInit_03.sh
+++ sdk/scripts/tradeInit_03.sh
@@ -1,7 +1,7 @@
-node "/home/ysjyx7/arbitrage-repo/sdk/TradeInit/loal-version/js/main3_0.js" --testCount=0.05 --ethCount=0.02
-node "/home/ysjyx7/arbitrage-repo/sdk/TradeInit/loal-version/js/main3_0.js" --testCount=0.05 --ethCount=0.02
-node "/home/ysjyx7/arbitrage-repo/sdk/TradeInit/loal-version/js/main3_2.js" --testCount=0.05 --ethCount=0.02
-node "/home/ysjyx7/arbitrage-repo/sdk/TradeInit/loal-version/js/main3_1.js" --testCount=0.05 --ethCount=0.02
-node "/home/ysjyx7/arbitrage-repo/sdk/TradeInit/loal-version/js/main3_0.js" --testCount=0.05 --ethCount=0.02
+node "../TradeInit/loal-version/js/main3_0.js" --testCount=0.05 --ethCount=0.02
+node "../TradeInit/loal-version/js/main3_0.js" --testCount=0.05 --ethCount=0.02
+node "../TradeInit/loal-version/js/main3_2.js" --testCount=0.05 --ethCount=0.02
+node "../TradeInit/loal-version/js/main3_1.js" --testCount=0.05 --ethCount=0.02
+node "../TradeInit/loal-version/js/main3_0.js" --testCount=0.05 --ethCount=0.02
 
 
