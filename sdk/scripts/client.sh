81d9106f0508437637ee69318866bda459fe96a5
Haodong Lu <79371536+SleepyLGod@users.noreply.github.com>
2022-10-11 15:22:22 +0800

    Update client.sh

--- sdk/scripts/client.sh
+++ sdk/scripts/client.sh
@@ -1,3 +1,3 @@
-cd /home/ysjyx7/arbitrage-repo/sdk/ArbitrageProject/version2.0/client
+cd ../ArbitrageProject/version2.0/client
 #go mod tidy
 go run listener.go Decodetxdata.go func1.go func2.go utils.go
